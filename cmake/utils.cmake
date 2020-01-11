175c697041c74c18e9208408f9e4f4de311a9a3a
Richard Musil <risa2000x@gmail.com>
2020-01-11 21:18:34 +0100

    Added CMake support for static and dynamic runtime.
    The external DLLs are copied into the build folder and packed into install archive.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -27,3 +27,56 @@ function (print_variables)
     endforeach()
     message (STATUS "print_variables------------------------------------------}")
 endfunction()
+
+# Find DLL specified in 'dllname' in 'bin' and 'lib' subpaths
+function (find_dll var dllname)
+    # check first the 'bin' paths
+    unset (_dllpath)
+    unset (_dllpath CACHE)
+    find_program (_dllpath ${dllname})
+    if (NOT _dllpath)
+        # now check the 'lib' paths
+        set (CMAKE_FIND_LIBRARY_SUFFIXES "")
+        find_library (_dllpath ${dllname})
+        set (CMAKE_FIND_LIBRARY_SUFFIXES .lib)
+        if (NOT _dllpath)
+            message (SEND_ERROR "find_dll(${var} ${dllname}) cannot find ${dllname}")
+        endif()
+    endif()
+    set (${var} ${_dllpath} PARENT_SCOPE)
+    message (STATUS "find_dll(${var} ${dllname}) -> ${_dllpath}")
+endfunction()
+
+# Find all DLLs specified after 'var' in 'bin' and 'lib' subpaths
+function (find_dlls var)
+    foreach (dll IN ITEMS ${ARGN})
+        find_dll (dllpath ${dll})
+        list (APPEND dllpaths ${dllpath})
+    endforeach()
+    set (${var} ${dllpaths} PARENT_SCOPE)
+endfunction()
+
+# Collect all DLLs for libs specified after 'tgt_name'
+#   libs:       are specified without any suffix
+#   tgt_name:   is the name of the target which requires the DLLs
+#   found_dlls: will be copied into the same folder as the target binary.
+function (collect_dlls tgt_name)
+    foreach (lib IN ITEMS ${ARGN})
+        add_library (${lib}_tgt SHARED IMPORTED)
+        find_dll(dllpath "${lib}d.dll")
+        set_property (TARGET ${lib}_tgt PROPERTY IMPORTED_LOCATION_DEBUG ${dllpath})
+        find_dll(dllpath "${lib}.dll")
+        set_property (TARGET ${lib}_tgt PROPERTY IMPORTED_LOCATION_RELEASE ${dllpath})
+        list (APPEND EXT_LIBS_DLLS "$<TARGET_FILE:${lib}_tgt>")
+        install (FILES "$<TARGET_FILE:${lib}_tgt>"
+            DESTINATION .
+            )
+    endforeach()
+
+    add_custom_command (TARGET ${tgt_name}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            ${EXT_LIBS_DLLS}
+            $<TARGET_FILE_DIR:${tgt_name}>
+        )
+endfunction()
