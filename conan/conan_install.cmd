f385694c0a66584f79ee20117275a414b85be597
Richard Musil <risa2000x@gmail.com>
2022-09-24 15:17:59 +0200

    Made the conan profile explicit.

--- conan/conan_install.cmd
+++ conan/conan_install.cmd
@@ -1,6 +1,6 @@
-conan install -if out/conan/x64-DLL-Debug -pr:h default -pr:b default -s compiler.runtime=MDd -s build_type=Debug .
-conan install -if out/conan/x64-DLL-Release -pr:h default -pr:b default -s compiler.runtime=MD -s build_type=Release .
-conan install -if out/conan/x64-Static-Debug -pr:h default -pr:b default -s compiler.runtime=MTd -s build_type=Debug .
-conan install -if out/conan/x64-Static-Release -pr:h default -pr:b default -s compiler.runtime=MT -s build_type=Release .
-conan install -if out/conan/x64-xClang-Static-Debug -pr:h default -pr:b default -s compiler.runtime=MTd -s build_type=Debug .
-conan install -if out/conan/x64-xClang-Static-Release -pr:h default -pr:b default -s compiler.runtime=MT -s build_type=Release .
+conan install -if out/conan/x64-DLL-Debug -pr:h ./conan/conan.profile.txt -pr:b ./conan/conan.profile.txt -s compiler.runtime=dynamic -s build_type=Debug .
+conan install -if out/conan/x64-DLL-Release -pr:h ./conan/conan.profile.txt -pr:b ./conan/conan.profile.txt -s compiler.runtime=dynamic -s build_type=Release .
+conan install -if out/conan/x64-Static-Debug -pr:h ./conan/conan.profile.txt -pr:b ./conan/conan.profile.txt -s compiler.runtime=static -s build_type=Debug .
+conan install -if out/conan/x64-Static-Release -pr:h ./conan/conan.profile.txt -pr:b ./conan/conan.profile.txt -s compiler.runtime=static -s build_type=Release .
+conan install -if out/conan/x64-xClang-Static-Debug -pr:h ./conan/conan.profile.txt -pr:b ./conan/conan.profile.txt -s compiler.runtime=static -s build_type=Debug .
+conan install -if out/conan/x64-xClang-Static-Release -pr:h ./conan/conan.profile.txt -pr:b ./conan/conan.profile.txt -s compiler.runtime=static -s build_type=Release .
